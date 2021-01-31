73ada4c8e4e05996f38b0cb3d1b68501b798fa84
Max <maks.dubovik.98@gmail.com>
2021-01-31 22:00:46 +0300

    new scroll

---
+++ js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.nav__menu'),
+    menuItem = document.querySelectorAll('.nav__item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('nav__menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('nav__menu_active');
+        })
+    })
+})
+
+$(document).ready(function(){
+
+    // Scroll
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function(){
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
+});
